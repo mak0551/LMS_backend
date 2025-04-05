6929697d842a4bd27bed4081f501dc104465949d
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-04-05 19:08:41 +0530

    default profile image

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema(
     profileImg: {
       type: String,
       default:
-        "https://images.unsplash.com/photo-1457449940276-e8deed18bfff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://cdn-icons-png.flaticon.com/512/12225/12225935.png",
     },
     isVerified: { type: Boolean, default: false },
     courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "course" }],
